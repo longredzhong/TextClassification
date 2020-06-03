e9ef52cbb3c0437f1b7127bfc2f54686fe54b931
longred <774931894@qq.com>
2020-06-03 09:54:49 +0800

    add
    Signed-off-by: longred <774931894@qq.com>

--- main.py
+++ main.py
@@ -4,15 +4,11 @@ import os
 import datetime
 from TextClassification.utils.cp_file2log import cp_file2log
 from torch.utils.tensorboard import SummaryWriter
-<<<<<<< HEAD
-from TextClassification.utils.Run_CB_loss import Run
-=======
 from TextClassification.utils.Run import Run
 from TextClassification.utils.Run_CB_loss import Run_CB_Loss
->>>>>>> 6df15067529deb85bda6e29cf1bb29d657c8f971
 if __name__ == "__main__":
     config = BaseConfig()
-    logdir = os.path.join("run_log", config.ModelName,
+    logdir = os.path.join(config.LogPath, config.ModelName,
                           str(datetime.datetime.now()))
     config.LogPath = logdir
     write = SummaryWriter(log_dir=logdir)
