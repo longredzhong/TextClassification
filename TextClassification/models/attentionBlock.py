fd7cac4290d9aaa1b902984c5aecde6d553b6609
longred <zch774931894@gmail.com>
2021-03-26 00:00:34 +0800

    add attention

---
+++ TextClassification/models/attentionBlock.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+import math
+import numpy as np
+import  torch.nn.functional as F
+
+class AttentionBlock(nn.Module):
+  """An attention mechanism similar to Vaswani et al (2017)
+  The input of the AttentionBlock is `BxTxD` where `B` is the input
+  minibatch size, `T` is the length of the sequence `D` is the dimensions of
+  each feature.
+  The output of the AttentionBlock is `BxTx(D+V)` where `V` is the size of the
+  attention values.
+  Arguments:
+      dims (int): the number of dimensions (or channels) of each element in
+          the input sequence
+      k_size (int): the size of the attention keys
+      v_size (int): the size of the attention values
+      seq_len (int): the length of the input and output sequences
+  """
+  def __init__(self, dims, k_size, v_size, seq_len=None):
+    super(AttentionBlock, self).__init__()
+    self.key_layer = nn.Linear(dims, k_size)
+    self.query_layer = nn.Linear(dims, k_size)
+    self.value_layer = nn.Linear(dims, v_size)
+    self.sqrt_k = math.sqrt(k_size)
+
+  def forward(self, minibatch):
+    keys = self.key_layer(minibatch)
+    queries = self.query_layer(minibatch)
+    values = self.value_layer(minibatch)
+    logits = torch.bmm(queries, keys.transpose(2,1))
+    # Use numpy triu because you can't do 3D triu with PyTorch
+    # TODO: using float32 here might break for non FloatTensor inputs.
+    # Should update this later to use numpy/PyTorch types of the input.
+    device = torch.device("cuda" if (torch.cuda.is_available()) else "cpu")
+    mask = np.triu(np.ones(logits.size()), k=1).astype('uint8')
+    mask = torch.from_numpy(mask).to(device)
+    # do masked_fill_ on data rather than Variable because PyTorch doesn't
+    # support masked_fill_ w/-inf directly on Variables for some reason.
+    logits.data.masked_fill_(mask, float('-inf'))
+    probs = F.softmax(logits, dim=1) / self.sqrt_k
+    read = torch.bmm(probs, values)
+    return minibatch + read
